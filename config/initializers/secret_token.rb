5e80deda5db1b7617ecc375400707781e7b4715b
adelaiderb <adelaiderb@michael-laptop.(none)>
2013-09-03 09:02:37 +0930

    Rails 4 Initializer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vote::Application.config.secret_key_base = '3130346cb7cc3a4b7788cb0eddd706a52d8931e245081e3c4f0f43535b83b2260b25af624bf4bc09dd8b68e4de90e4bb17856cf326ad3d30b7f724efb0868f0d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vote::Application.config.session_store :cookie_store, key: '_vote_session'
